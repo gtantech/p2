9d5fe9f7c72a15951f3173f88323614880b1f3a7
gtantech <43525064+gtantech@users.noreply.github.com>
2026-09-19 14:20:30 -0400

    Added store service (#4)
    * Added project store
    
    * Added activity store
    
    * Added dependency store
    
    * Added Get function to DependencyStore
    
    * Fixed comment formatting
    
    * Added ErrActivityNotFound for sql.ErrNoRows
    
    * Added ErrDependencyNotFound for sql.ErrNoRows
    
    * Added ErrProjectNotFound for sql.ErrNoRows

---
+++ internal/store/activity.go
@@ -0,0 +1,142 @@
+package store
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"time"
+	"uuid"
+
+	"github.com/gtantech/p2/internal/db"
+)
+
+var ErrActivityNotFound = errors.New("activity not found")
+
+type Activity struct {
+	ID          uuid.UUID
+	ProjectID   uuid.UUID
+	DisplayName string
+	Duration    time.Duration
+}
+
+func newActivity(id uuid.UUID, projectId uuid.UUID, displayName string, duration time.Duration) Activity {
+	return Activity{ID: id, ProjectID: projectId, DisplayName: displayName, Duration: duration}
+}
+
+type CreateActivityDbParams struct {
+	ProjectID   uuid.UUID
+	DisplayName string
+	Duration    time.Duration
+}
+
+type GetByNameAndProjectParams struct {
+	ProjectID   uuid.UUID
+	DisplayName string
+}
+
+type UpdateActivityDbParams struct {
+	Id          uuid.UUID
+	DisplayName string
+	Duration    time.Duration
+}
+
+type ActivityStore interface {
+	GetByProjectID(ctx context.Context, projectId uuid.UUID) ([]Activity, error)
+	GetByNameAndProject(ctx context.Context, params GetByNameAndProjectParams) ([]Activity, error)
+	GetByID(ctx context.Context, id uuid.UUID) (Activity, error)
+	Create(ctx context.Context, params CreateActivityDbParams) (Activity, error)
+	Update(ctx context.Context, params UpdateActivityDbParams) (Activity, error)
+	Delete(ctx context.Context, id uuid.UUID) error
+}
+
+type activityDbStore struct {
+	queries *db.Queries
+}
+
+// Create implements [ActivityStore].
+func (a *activityDbStore) Create(ctx context.Context, params CreateActivityDbParams) (Activity, error) {
+	data, err := a.queries.InsertActivity(ctx, db.InsertActivityParams{
+		ID:        uuid.NewV7().String(),
+		ProjectID: params.ProjectID.String(),
+		DispName:  params.DisplayName,
+		Duration:  int64(params.Duration),
+	})
+	if err != nil {
+		return Activity{}, err
+	}
+	return newActivity(uuid.MustParse(data.ID), uuid.MustParse(data.ProjectID), data.DispName, time.Duration(data.Duration)), nil
+}
+
+// Delete implements [ActivityStore].
+func (a *activityDbStore) Delete(ctx context.Context, id uuid.UUID) error {
+	return a.queries.DeleteActivity(ctx, id.String())
+}
+
+// GetByID implements [ActivityStore].
+func (a *activityDbStore) GetByID(ctx context.Context, id uuid.UUID) (Activity, error) {
+	data, err := a.queries.FindActivityById(ctx, id.String())
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			// Activity doesn't exist
+			return Activity{}, ErrActivityNotFound
+		}
+		return Activity{}, err
+	}
+	return newActivity(uuid.MustParse(data.ID), uuid.MustParse(data.ProjectID), data.DispName, time.Duration(data.Duration)), nil
+}
+
+// GetByNameAndProject implements [ActivityStore].
+func (a *activityDbStore) GetByNameAndProject(ctx context.Context, params GetByNameAndProjectParams) ([]Activity, error) {
+	data, err := a.queries.FindAllActivitiesByNameAndProject(ctx, db.FindAllActivitiesByNameAndProjectParams{
+		DispName:  params.DisplayName,
+		ProjectID: params.ProjectID.String(),
+	})
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			// Activity doesn't exist
+			return []Activity{}, ErrActivityNotFound
+		}
+		return []Activity{}, err
+	}
+	activities := make([]Activity, len(data))
+	for i, d := range data {
+		activities[i] = newActivity(uuid.MustParse(d.ID), uuid.MustParse(d.ProjectID), d.DispName, time.Duration(d.Duration))
+	}
+	return activities, nil
+}
+
+// GetByProjectID implements [ActivityStore].
+func (a *activityDbStore) GetByProjectID(ctx context.Context, projectId uuid.UUID) ([]Activity, error) {
+	data, err := a.queries.FindAllActivitiesByProject(ctx, projectId.String())
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			// Activity doesn't exist
+			return []Activity{}, ErrActivityNotFound
+		}
+		return []Activity{}, err
+	}
+	activities := make([]Activity, len(data))
+	for i, d := range data {
+		activities[i] = newActivity(uuid.MustParse(d.ID), uuid.MustParse(d.ProjectID), d.DispName, time.Duration(d.Duration))
+	}
+	return activities, nil
+}
+
+// Update implements [ActivityStore].
+func (a *activityDbStore) Update(ctx context.Context, params UpdateActivityDbParams) (Activity, error) {
+	data, err := a.queries.UpdateActivity(ctx, db.UpdateActivityParams{
+		DispName: params.DisplayName,
+		Duration: int64(params.Duration),
+		ID:       params.Id.String(),
+	})
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			// Activity doesn't exist
+			return Activity{}, ErrActivityNotFound
+		}
+		return Activity{}, err
+	}
+	return newActivity(uuid.MustParse(data.ID), uuid.MustParse(data.ProjectID), data.DispName, time.Duration(data.Duration)), nil
+}
+
+var _ ActivityStore = (*activityDbStore)(nil) //ensures activityDbStore implements ActivityStore at compile time
---
+++ internal/store/dependency.go
@@ -0,0 +1,180 @@
+package store
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"uuid"
+
+	"github.com/gtantech/p2/internal/db"
+)
+
+type RelationshipType string
+
+var ErrDependencyNotFound = errors.New("dependency not found")
+
+const (
+	SS RelationshipType = "SS" // start to start relationship
+	FS RelationshipType = "FS" // finish to start relationship
+	SF RelationshipType = "SF" // start to finish relationship
+	FF RelationshipType = "FF" // finish to finish relationship
+)
+
+type Dependency struct {
+	ID                    uuid.UUID
+	ProjectID             uuid.UUID
+	Relationship          RelationshipType
+	PredecessorActivityID uuid.UUID
+	SuccessorActivityID   uuid.UUID
+}
+
+func newDependency(id uuid.UUID, projectId uuid.UUID, relationship RelationshipType, predecessorActivityId uuid.UUID, successorActivityId uuid.UUID) Dependency {
+	return Dependency{
+		ID:                    id,
+		ProjectID:             projectId,
+		Relationship:          relationship,
+		PredecessorActivityID: predecessorActivityId,
+		SuccessorActivityID:   successorActivityId,
+	}
+}
+
+type CreateDepdencencyDbParams struct {
+	ProjectID             uuid.UUID
+	Relationship          RelationshipType
+	PredecessorActivityID uuid.UUID
+	SuccessorActivityID   uuid.UUID
+}
+
+type UpdateDepdencencyDbParams struct {
+	ID                    uuid.UUID
+	Relationship          RelationshipType
+	PredecessorActivityID uuid.UUID
+	SuccessorActivityID   uuid.UUID
+}
+
+type DependencyStore interface {
+	GetByID(ctx context.Context, id uuid.UUID) (Dependency, error)
+	GetByProjectID(ctx context.Context, projectId uuid.UUID) ([]Dependency, error)
+	GetByPredecessor(ctx context.Context, predecessorId uuid.UUID) ([]Dependency, error)
+	GetBySuccessor(ctx context.Context, successorId uuid.UUID) ([]Dependency, error)
+	Create(ctx context.Context, params CreateDepdencencyDbParams) (Dependency, error)
+	Update(ctx context.Context, params UpdateDepdencencyDbParams) (Dependency, error)
+	Delete(ctx context.Context, id uuid.UUID) error
+}
+
+type dependencyDbStore struct {
+	queries *db.Queries
+}
+
+// GetByProjectID implements [DependencyStore].
+func (d *dependencyDbStore) GetByProjectID(ctx context.Context, projectId uuid.UUID) ([]Dependency, error) {
+	data, err := d.queries.FindAllDependenciesByProject(ctx, projectId.String())
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			// Dependency doesn't exist
+			return []Dependency{}, ErrDependencyNotFound
+		}
+		return []Dependency{}, err
+	}
+
+	dependencies := make([]Dependency, len(data))
+	for i, d := range data {
+		dependencies[i] = newDependency(uuid.MustParse(d.ID), uuid.MustParse(d.ProjectID), RelationshipType(d.Relationship), uuid.MustParse(d.PredecessorActivityID), uuid.MustParse(d.SuccessorActivityID))
+	}
+	return dependencies, nil
+}
+
+// GetByID implements [DependencyStore].
+func (d *dependencyDbStore) GetByID(ctx context.Context, id uuid.UUID) (Dependency, error) {
+	data, err := d.queries.FindDependencyById(ctx, id.String())
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			// Dependency doesn't exist
+			return Dependency{}, ErrDependencyNotFound
+		}
+		return Dependency{}, err
+	}
+	return newDependency(uuid.MustParse(data.ID), uuid.MustParse(data.ProjectID), RelationshipType(data.Relationship), uuid.MustParse(data.PredecessorActivityID), uuid.MustParse(data.SuccessorActivityID)), nil
+}
+
+// GetByPredecessor implements [DependencyStore].
+func (d *dependencyDbStore) GetByPredecessor(ctx context.Context, predecessorId uuid.UUID) ([]Dependency, error) {
+	data, err := d.queries.FindAllDependenciesByPredecessor(ctx, predecessorId.String())
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			// Dependency doesn't exist
+			return []Dependency{}, ErrDependencyNotFound
+		}
+		return []Dependency{}, err
+	}
+
+	dependencies := make([]Dependency, len(data))
+	for i, d := range data {
+		dependencies[i] = newDependency(uuid.MustParse(d.ID), uuid.MustParse(d.ProjectID), RelationshipType(d.Relationship), uuid.MustParse(d.PredecessorActivityID), uuid.MustParse(d.SuccessorActivityID))
+	}
+	return dependencies, nil
+}
+
+// GetBySuccessor implements [DependencyStore].
+func (d *dependencyDbStore) GetBySuccessor(ctx context.Context, successorId uuid.UUID) ([]Dependency, error) {
+	data, err := d.queries.FindAllDependenciesBySuccessor(ctx, successorId.String())
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			// Dependency doesn't exist
+			return []Dependency{}, ErrDependencyNotFound
+		}
+		return []Dependency{}, err
+	}
+
+	dependencies := make([]Dependency, len(data))
+	for i, d := range data {
+		dependencies[i] = newDependency(uuid.MustParse(d.ID), uuid.MustParse(d.ProjectID), RelationshipType(d.Relationship), uuid.MustParse(d.PredecessorActivityID), uuid.MustParse(d.SuccessorActivityID))
+	}
+	return dependencies, nil
+}
+
+// Create implements [DependencyStore].
+func (d *dependencyDbStore) Create(ctx context.Context, params CreateDepdencencyDbParams) (Dependency, error) {
+	data, err := d.queries.InsertDependency(ctx, db.InsertDependencyParams{
+		ID:                    uuid.NewV7().String(),
+		ProjectID:             params.ProjectID.String(),
+		Relationship:          string(params.Relationship),
+		PredecessorActivityID: params.PredecessorActivityID.String(),
+		SuccessorActivityID:   params.SuccessorActivityID.String(),
+	})
+	if err != nil {
+		return Dependency{}, err
+	}
+	return newDependency(uuid.MustParse(data.ID), uuid.MustParse(data.ProjectID), RelationshipType(data.Relationship), uuid.MustParse(data.PredecessorActivityID), uuid.MustParse(data.SuccessorActivityID)), nil
+}
+
+// Delete implements [DependencyStore].
+func (d *dependencyDbStore) Delete(ctx context.Context, id uuid.UUID) error {
+	return d.queries.DeleteDependency(ctx, id.String())
+}
+
+// Update implements [DependencyStore].
+func (d *dependencyDbStore) Update(ctx context.Context, params UpdateDepdencencyDbParams) (Dependency, error) {
+	data, err := d.queries.UpdateDependency(ctx, db.UpdateDependencyParams{
+		Relationship:          string(params.Relationship),
+		PredecessorActivityID: params.PredecessorActivityID.String(),
+		SuccessorActivityID:   params.SuccessorActivityID.String(),
+		ID:                    params.ID.String(),
+	})
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			// Dependency doesn't exist
+			return Dependency{}, ErrDependencyNotFound
+		}
+		return Dependency{}, err
+	}
+	return newDependency(uuid.MustParse(data.ID), uuid.MustParse(data.ProjectID), RelationshipType(data.Relationship), uuid.MustParse(data.PredecessorActivityID), uuid.MustParse(data.SuccessorActivityID)), nil
+}
+
+func NewDependencyDbStore(queries *db.Queries) *dependencyDbStore {
+	return &dependencyDbStore{
+		queries: queries,
+	}
+}
+
+var _ DependencyStore = (*dependencyDbStore)(nil) //ensures dependencyDbStore implements DependencyStore at compile time
