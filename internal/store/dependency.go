3d432b40b5002cbe9c5ec9d6372e10918c54e077
gtantech <tangabrielkl@gmail.com>
2026-09-19 14:06:36 -0400

    Fixed comment formatting

--- internal/store/dependency.go
+++ internal/store/dependency.go
@@ -13,7 +13,7 @@ const (
 	SS RelationshipType = "SS" // start to start relationship
 	FS RelationshipType = "FS" // finish to start relationship
 	SF RelationshipType = "SF" // start to finish relationship
-	FF RelationshipType = "FF" //finish to finish relationship
+	FF RelationshipType = "FF" // finish to finish relationship
 )
 
 type Dependency struct {
