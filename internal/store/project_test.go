202a02d937e7c7458c935996abafbfb01d2f0f59
gtantech <43525064+gtantech@users.noreply.github.com>
2026-09-20 13:23:24 -0400

    Added store package unit tests (#10)
    * Added activity unit tests
    
    * Added dependency unit tests
    
    * Added project unit tests

---
+++ internal/store/project_test.go
@@ -0,0 +1,71 @@
+package store
+
+import (
+	"testing"
+	"uuid"
+
+	"github.com/gtantech/p2/internal/db"
+)
+
+func TestNewProject(t *testing.T) {
+	projectId := uuid.New()
+	displayName := "test_disp_name"
+
+	p := newProject(projectId, displayName)
+
+	if got, want := p.ID, projectId; got != want {
+		t.Errorf("got %v, want %v", got, want)
+	}
+
+	if got, want := p.DisplayName, displayName; got != want {
+		t.Errorf("got %v, want %v", got, want)
+	}
+}
+
+func TestToDbInsertProjectParams(t *testing.T) {
+	projectId := uuid.New()
+	displayName := "test_disp_name"
+
+	params := CreateProjectParams{
+		DisplayName: displayName,
+	}
+
+	dbParams := params.ToDbInsertProjectParams(projectId)
+
+	if got, want := uuid.MustParse(dbParams.ID), projectId; got != want {
+		t.Errorf("got %v, want %v", got, want)
+	}
+
+	if got, want := dbParams.DispName, displayName; got != want {
+		t.Errorf("got %v, want %v", got, want)
+	}
+}
+
+func TestToDbUpdateProjectParams(t *testing.T) {
+	projectId := uuid.New()
+	displayName := "test_disp_name"
+
+	params := UpdateProjectParams{
+		Id:          projectId,
+		DisplayName: displayName,
+	}
+
+	dbParams := params.ToDbUpdateProjectParams()
+
+	if got, want := uuid.MustParse(dbParams.ID), projectId; got != want {
+		t.Errorf("got %v, want %v", got, want)
+	}
+
+	if got, want := dbParams.DispName, displayName; got != want {
+		t.Errorf("got %v, want %v", got, want)
+	}
+}
+
+func TestNewProjectStoreFromDb(t *testing.T) {
+	var q = &db.Queries{}
+	s := NewProjectStoreFromDb(q)
+
+	if got, want := s.queries, q; got != want {
+		t.Errorf("got %v, want %v", got, want)
+	}
+}
