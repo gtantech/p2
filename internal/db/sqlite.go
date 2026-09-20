21bd75012c4477a1d880001e1035c205447d976f
gtantech <43525064+gtantech@users.noreply.github.com>
2026-09-20 08:45:39 -0400

    Added sqlite (#5)
    * Moved sql related files to db
    
    * Added sqlite
    
    * Renamed path param in favour of dataSourceName
    
    * Moved database driver param to caller for migrateDB

---
+++ internal/db/sqlite.go
@@ -0,0 +1,58 @@
+package db
+
+import (
+	"database/sql"
+	"embed"
+	"errors"
+	"log"
+
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database"
+	"github.com/golang-migrate/migrate/v4/database/sqlite"
+	"github.com/golang-migrate/migrate/v4/source/iofs"
+	_ "modernc.org/sqlite"
+)
+
+//go:embed migrations/*.sql
+var migrationsFS embed.FS
+
+func NewSQLiteStorage(dataSourceName string) (*sql.DB, error) {
+	db, err := sql.Open("sqlite", dataSourceName)
+	if err != nil {
+		log.Fatalf("Failed to open database: %v", err)
+	}
+
+	// Verify the connection is working
+	if err := db.Ping(); err != nil {
+		log.Fatalf("Failed to ping database: %v", err)
+	}
+	driver, err := sqlite.WithInstance(db, &sqlite.Config{})
+	if err != nil {
+		return nil, err
+	}
+	migrateDB(driver)
+	return db, nil
+}
+
+func migrateDB(driver database.Driver) error {
+	source, err := iofs.New(migrationsFS, "migrations")
+	if err != nil {
+		return err
+	}
+
+	m, err := migrate.NewWithInstance(
+		"iofs",
+		source,
+		"main",
+		driver,
+	)
+	if err != nil {
+		return err
+	}
+
+	if err := m.Up(); err != nil && !errors.Is(err, migrate.ErrNoChange) {
+		return err
+	}
+
+	return nil
+}
