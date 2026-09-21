e4c69cd1d9e8e8ea112b2b12b0bf2d51e792ac46
gtantech <43525064+gtantech@users.noreply.github.com>
2026-09-20 23:26:34 -0400

    Registered DisplayDependenciesToAdd to routes (#17)
    * Squashed commit of the following:
    
    commit 76a2f537e7087dfac0feb34d48e47adf0ec3e704
    Author: gtantech <tangabrielkl@gmail.com>
    Date:   Sun Sep 20 22:38:07 2026 -0400
    
        Added DisplayDependenciesToAdd
    
    commit c4bc642be34a5ca86555202c775cd5b98ef636d8
    Author: gtantech <tangabrielkl@gmail.com>
    Date:   Sun Sep 20 22:22:41 2026 -0400
    
        Unexported activity
    
    commit 1e34e28f74abdac3cc74d8b3001fcbfd43cddd92
    Author: gtantech <tangabrielkl@gmail.com>
    Date:   Sun Sep 20 21:42:16 2026 -0400
    
        Added activity
    
    * Added DisplayDependenciesToAdd
    
    * Registered DisplayDependenciesToAdd to routes

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -1,7 +1,10 @@
 package routes
 
 import (
+	"errors"
+	"log"
 	"net/http"
+	"uuid"
 
 	"github.com/a-h/templ"
 	"github.com/gtantech/p2/internal/store"
@@ -26,4 +29,34 @@ func renderTemplComponent(component templ.Component, w http.ResponseWriter, r *h
 
 func (rt *Routes) HelloWorldHandler(w http.ResponseWriter, r *http.Request) {
 	renderTemplComponent(rt.view.Home(), w, r)
+}
+
+func (rt *Routes) DisplayDependenciesToAdd(w http.ResponseWriter, r *http.Request) {
+	search := r.URL.Query().Get("search")
+	if search == "" {
+		http.Error(w, "missing search parameter", http.StatusBadRequest)
+		return
+	}
+	projectId, err := uuid.Parse(r.URL.Query().Get("project-id"))
+	if err != nil {
+		http.Error(w, "invalid project id parameter", http.StatusBadRequest)
+		return
+	}
+	successorId, err := uuid.Parse(r.URL.Query().Get("successor-id"))
+
+	if err != nil {
+		http.Error(w, "invalid successor id parameter", http.StatusBadRequest)
+		return
+	}
+	component, err := rt.view.DisplayDependenciesToAdd(r.Context(), search, projectId, successorId)
+	if err != nil {
+		if errors.Is(err, view.ErrActivityNotFound) {
+			http.Error(w, "activity not found", http.StatusNotFound)
+			return
+		}
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		log.Printf("internal server error occurred with error:%v\n", err)
+		return
+	}
+	renderTemplComponent(component, w, r)
 }
