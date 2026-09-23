db94b39ec06d58a97f08d35c6168f63dc8978b44
gtantech <tangabrielkl@gmail.com>
2026-09-22 21:23:50 -0400

    Moved view models to separate file

--- internal/view/models.go
+++ internal/view/models.go
@@ -15,3 +15,20 @@ type Activity struct {
 func NewActivity(id uuid.UUID, projectId uuid.UUID, displayName string, duration time.Duration) *Activity {
 	return &Activity{id: id, projectID: projectId, displayName: displayName, duration: duration}
 }
+
+type Table struct {
+	rows []*TableRow
+}
+
+func NewTable() *Table {
+	return &Table{rows: []*TableRow{}}
+}
+
+type TableRow struct {
+	activity     *Activity
+	dependencies []*Activity
+}
+
+func NewTableRow(activity *Activity, dependencies []*Activity) *TableRow {
+	return &TableRow{activity: activity, dependencies: dependencies}
+}
