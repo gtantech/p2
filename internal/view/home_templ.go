fa5768c7ceeabb1b6380b7131c3d918da50e264b
gtantech <tangabrielkl@gmail.com>
2026-09-23 10:46:15 -0400

    Added htmx script tag

--- internal/view/home_templ.go
+++ internal/view/home_templ.go
@@ -29,7 +29,7 @@ func home(table *Table) templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<!doctype html><html lang=\"en\"><head><meta charset=\"UTF-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"><title>Task Planner</title><link rel=\"stylesheet\" href=\"/static/home_style.css\"></head><body><div class=\"container\"><h1>Task Planner</h1><div class=\"table-wrapper\">")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<!doctype html><html lang=\"en\"><head><meta charset=\"UTF-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"><title>Task Planner</title><link rel=\"stylesheet\" href=\"/static/home_style.css\"><script src=\"https://cdn.jsdelivr.net/npm/htmx.org@2.0.10/dist/htmx.min.js\" integrity=\"sha384-H5SrcfygHmAuTDZphMHqBJLc3FhssKjG7w/CeCpFReSfwBWDTKpkzPP8c+cLsK+V\" crossorigin=\"anonymous\">\r\n\t\t  \t</script></head><body><div class=\"container\"><h1>Task Planner</h1><div class=\"table-wrapper\">")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
