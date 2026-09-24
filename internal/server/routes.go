e69ec3dfa041c1a4e73b74733b61dd399240b854
gtantech <tangabrielkl@gmail.com>
2026-09-24 17:58:39 -0400

    Added /table as a subroute to /api

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -31,10 +31,12 @@ func (s *Server) RegisterRoutes(routes *routes.Routes) http.Handler {
 		r.Get("/table/dependency/row/empty", routes.GetEmptyTableRow)
 	})
 
-	r.Route("/table", func(r chi.Router) {
-		r.Post("/row/activity/{id}/name", routes.PostActivityNameUpdateFromTableHandler)
-		r.Post("/row/activity/{id}/dependency", routes.PostActivityDependencyUpdateFromTableHandler)
-		r.Post("/row/activity/{id}/duration", routes.PostActivityDurationUpdateFromTableHandler)
+	r.Route("/api", func(r chi.Router) {
+		r.Route("/table", func(r chi.Router) {
+			r.Post("/row/activity/{id}/name", routes.PostActivityNameUpdateFromTableHandler)
+			r.Post("/row/activity/{id}/dependency", routes.PostActivityDependencyUpdateFromTableHandler)
+			r.Post("/row/activity/{id}/duration", routes.PostActivityDurationUpdateFromTableHandler)
+		})
 	})
 
 	r.Get("/static/home_style.css", routes.GetHomeStyle)
