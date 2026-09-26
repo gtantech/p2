a031d15b8579d0d48a9072f86705ab4055351392
gtantech <tangabrielkl@gmail.com>
2026-09-26 01:11:33 -0400

    Moved to view.go

--- internal/view/view.go
+++ internal/view/view.go
@@ -4,6 +4,7 @@ import (
 	json "encoding/json/v2"
 	"errors"
 	"fmt"
+	"time"
 	"uuid"
 
 	"github.com/a-h/templ"
@@ -34,4 +35,43 @@ func marshalParams(in any) string {
 		panic(fmt.Sprintf("failed to marshal json from params: %v", in))
 	}
 	return string(out)
+}
+
+func NewActivity(id uuid.UUID, projectId uuid.UUID, displayName string, duration time.Duration) *models.ViewActivity {
+	return &models.ViewActivity{Id: id, ProjectID: projectId, DisplayName: displayName, Duration: duration}
+}
+
+func NewTable(rows []*models.ViewTableRow) *models.ViewTable {
+	return &models.ViewTable{Rows: rows}
+}
+
+func NewTableFromStorage(storeActivities []models.StoreActivity, storeDependencies map[models.StoreActivity][]models.StoreActivity) *models.ViewTable {
+	storeActivityMap := make(map[models.StoreActivity]*models.ViewActivity)
+
+	for _, storeActivity := range storeActivities {
+		//convert activity
+		storeActivityMap[storeActivity] = NewActivity(storeActivity.ID, storeActivity.ProjectID, storeActivity.DisplayName, storeActivity.Duration)
+	}
+
+	table := models.ViewTable{}
+
+	for _, storeActivity := range storeActivities {
+		viewActivity := storeActivityMap[storeActivity]
+		storeDependency := storeDependencies[storeActivity]
+		viewDependency := make([]*models.ViewActivity, len(storeDependency))
+		for i, predecessorActivity := range storeDependency {
+			viewDependency[i] = storeActivityMap[predecessorActivity]
+		}
+		table.Rows = append(table.Rows, NewTableRow(viewActivity, viewDependency))
+	}
+
+	return &table
+}
+
+func toPostEmptyTableRow(t *models.ViewTableRow) models.PostEmptyTableRow {
+	return models.PostEmptyTableRow{ProjectId: t.Activity.ProjectID}
+}
+
+func NewTableRow(activity *models.ViewActivity, dependencies []*models.ViewActivity) *models.ViewTableRow {
+	return &models.ViewTableRow{Activity: activity, Dependencies: dependencies}
 }
