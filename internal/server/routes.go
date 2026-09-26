f1ceb0f3559f07d772b36c03a3428afd16357fe5
gtantech <43525064+gtantech@users.noreply.github.com>
2026-09-25 23:43:02 -0400

    Fixed confusing path for PostEmptyTableRow (#37)

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -24,7 +24,7 @@ func (s *Server) RegisterRoutes(routes *routes.Routes) http.Handler {
 	r.Get("/", routes.HelloWorldHandler)
 
 	r.Route("/component", func(r chi.Router) {
-		r.Post("/table/dependency/row/empty", routes.PostEmptyTableRow)
+		r.Post("/table/row/empty", routes.PostEmptyTableRow)
 	})
 
 	r.Route("/api", func(r chi.Router) {
