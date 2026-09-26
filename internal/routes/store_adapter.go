8206ad2272dc23f31e11713f614c14b234750af4
gtantech <tangabrielkl@gmail.com>
2026-09-25 23:26:38 -0400

    Added storeAdapter

---
+++ internal/routes/store_adapter.go
@@ -0,0 +1,24 @@
+package routes
+
+import "github.com/gtantech/p2/internal/store"
+
+type storeAdapter struct {
+	store *store.Store
+}
+
+// Activity implements [StoreService].
+func (s *storeAdapter) Activity() ActivityStoreService {
+	return s.store.Activity
+}
+
+// Dependency implements [StoreService].
+func (s *storeAdapter) Dependency() DependencyStoreService {
+	return s.store.Dependency
+}
+
+// Project implements [StoreService].
+func (s *storeAdapter) Project() ProjectStoreService {
+	return s.store.Project
+}
+
+var _ StoreService = (*storeAdapter)(nil) //ensures ExampleStruct implements ExampleInterface at compile time
