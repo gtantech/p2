070651ddfc10cfbb35f3a675c50fd8912dd5dbb2
gtantech <43525064+gtantech@users.noreply.github.com>
2026-09-25 22:07:47 -0400

    Removed DisplayDependenciesToAdd (#35)

--- internal/view/view.go
+++ internal/view/view.go
@@ -1,7 +1,6 @@
 package view
 
 import (
-	"context"
 	json "encoding/json/v2"
 	"errors"
 	"fmt"
@@ -25,30 +24,6 @@ func NewView(store *store.Store) *View {
 
 func (v *View) Home(table *Table, homeProjectId uuid.UUID) templ.Component {
 	return home(table, homeProjectId)
-}
-
-func (v *View) DisplayDependenciesToAdd(ctx context.Context, query string, projectId uuid.UUID, successorId uuid.UUID) (templ.Component, error) {
-	storeActivities, err := v.store.Activity.GetByNameAndProject(ctx, store.GetActivityByNameAndProjectParams{ProjectID: projectId, DisplayName: query})
-	if err != nil {
-		if errors.Is(err, store.ErrActivityNotFound) {
-			// activity doesn't exist
-			return displayDependenciesToAddNotFound(), nil
-		}
-		return nil, err
-	}
-
-	activities := make([]Activity, len(storeActivities))
-
-	for i, storeActivity := range storeActivities {
-		activities[i] = Activity{
-			id:          storeActivity.ID,
-			projectID:   storeActivity.ProjectID,
-			displayName: storeActivity.DisplayName,
-			duration:    storeActivity.Duration,
-		}
-	}
-
-	return displayDependenciesToAdd(activities, successorId.String()), nil
 }
 
 func (v *View) DisplayEmptyTableRow(params DisplayEmptyTableRowParams) templ.Component {
